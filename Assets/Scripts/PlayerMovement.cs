13dfc20d90d5cb6e1dc3148249d9255ff7c15120
Michael <127899373+MJR-Michael@users.noreply.github.com>
2025-10-29 15:45:25 -0400

    Player movement implemented with position limits.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,13 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float controlSpeed;
+    [SerializeField] private float xClampRange;
+
+    // we have specific negY and posY clamps because the ship is not centered in the screen in terms of y.
+    [SerializeField] private float negYClampRange;
+    [SerializeField] private float posYClampRange;
+
+
     Vector2 movement;
 
     void Update()
@@ -19,8 +26,14 @@ public class PlayerMovement : MonoBehaviour
     private void ProcessTranslation()
     {
         float xOffSet = movement.x * controlSpeed * Time.deltaTime;
-        float yOffSet = movement.y * controlSpeed * Time.deltaTime;
+        float rawXPos = transform.localPosition.x + xOffSet;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xClampRange, xClampRange);
 
-        transform.localPosition = new Vector3(transform.localPosition.x + xOffSet, transform.localPosition.y + yOffSet, 0f);
+        float yOffSet = movement.y * controlSpeed * Time.deltaTime;
+        float rawYPos = transform.localPosition.y + yOffSet;
+        float clampedYPos = Mathf.Clamp(rawYPos, negYClampRange, posYClampRange);
+
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, 0f);
     }
 }
