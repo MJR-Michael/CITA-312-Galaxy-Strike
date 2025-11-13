fe20941e9887e784014109d61c23d1340f6cc227
Michael <127899373+MJR-Michael@users.noreply.github.com>
2025-11-13 08:54:22 -0500

    Instantiate at Runtime
    Created a particle system explosion when enemies are shot at with the laser.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,8 +3,10 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] GameObject destroyedShipVFX;
     private void OnParticleCollision(GameObject other)
     {
+        Instantiate(destroyedShipVFX, transform.position, Quaternion.identity);
         Destroy(this.gameObject);
     }
 }
